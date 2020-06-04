74ce89199e2d8d1ccd460976805697b3606a90fa
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-06-04 09:51:26 -0700

    app tasks

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,30 @@
 def word_count(s):
     # Your code here
+    cache = {}
+    text = ""
+    punctuation = ['"', ':', ';', ',', '.', '-', '+', '=', '/',
+                   '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    for c in s:
+        if c in punctuation:
+            s.replace(c, '')
+        else:
+            text += c
 
+    words = text.split()
+
+    for word in words:
+        word = word.lower()
+        if word in cache:
+            cache[word] += 1
+        else:
+            cache[word] = 1
+
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
